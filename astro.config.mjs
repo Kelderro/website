0b051e866dec1aada4802ba191f84e8ec71c2b28
Rob op den Kelder <Rob.opdenKelder@transavia.com>
2024-11-01 23:54:57 +0100

    Update to the latest NPM packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import image from '@astrojs/image';
+import mdx from '@astrojs/mdx';
 
-// https://astro.build/config
 export default defineConfig({
-    integrations: [image({
-        serviceEntryPoint: '@astrojs/image/sharp'
-    })],
+    integrations: [
+    mdx()],
 });
